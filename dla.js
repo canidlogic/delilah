94b5b20a276941d6339a5300c77a7b91513eafe8
Noah Johnson <noah.johnson@loupmail.com>
2022-01-03 10:59:26 -0600

    Add rendering support

--- dla.js
+++ dla.js
@@ -18,15 +18,41 @@
    */
   
   /*
+   * Render the current scene using a 2D canvas rendering context.
+   * 
+   * Both width and height must be at least two.
+   * 
+   * Parameters:
+   * 
+   *   rc : CanvasRenderingContext2D - the 2D rendering context
+   * 
+   *   w : integer - the width in pixels of the canvas
+   * 
+   *   h : integer - the height in pixels of the canvas
+   */
+  function renderScene(rc, w, h) {
+    // @@TODO:
+    rc.fillStyle = "rgb(128, 128, 255)";
+    rc.fillRect(0, 0, w, h);
+  }
+  
+  /*
    * Load a scene from a given string.
+   * 
+   * If false is returned, the current scene is unmodified
    * 
    * Parameters:
    * 
    *   str : string - the scene file contents to load
+   * 
+   * Return:
+   * 
+   *   true if successful, false if scene text has syntax error
    */
   function loadScene(str) {
     // @@TODO:
     console.log("loadScene");
+    return true;
   }
   
   /*
@@ -44,6 +70,7 @@
    * All exports are declared within a global "dla_main" object.
    */
   window.dla_main = {
+    "renderScene": renderScene,
     "loadScene": loadScene,
     "loadDefaultScene": loadDefaultScene
   };
