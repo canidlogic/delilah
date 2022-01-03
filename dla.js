651b412650f05257efb44882c2dcb702688d8cb8
Noah Johnson <noah.johnson@loupmail.com>
2022-01-03 13:54:06 -0600

    Declare internal data structures

--- dla.js
+++ dla.js
@@ -13,6 +13,216 @@
 (function() {
   
   /*
+   * Local data
+   * ==========
+   */
+  
+  /*
+   * Flag indicating whether a scene is loaded.
+   */
+  var m_loaded = false;
+  
+  /*
+   * The vertex buffer.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a Float64Array with (n * 3) elements, where n is in the
+   * range [1, 65535].  Each three elements is the (X, Y, Z) coordinate
+   * of a vertex.  Vertex n starts at index (n * 3), where vertex zero
+   * is the first vertex in the array.
+   */
+  var m_vtx;
+  
+  /*
+   * Transformed vertex buffer.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a Float64Array with the same number of elements as m_vtx.
+   * At the start of a rendering operation, all vertices in m_vtx will
+   * be transformed according to the current view matrix and the
+   * transformed results will be stored in this array.  The
+   * transformation does NOT include projection, however.
+   */
+  var m_tvx;
+  
+  /*
+   * The scene object array.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a Uint16Array with (n * 5) elements, where n is in the
+   * range [1, 65535].  Each five elements define a 3D object in the
+   * scene, as described below.  Object n starts at index (n * 5), where
+   * object zero is the first object in the array.
+   * 
+   * The five 16-bit words have the following meaning:
+   * 
+   *   1 : vertex index of first vertex
+   *   2 : vertex index of second vertex, or 0xffff if point
+   *   3 : vertex index of third vertex, or 0xffff if point or line
+   *   4 : fill color if triangle, else ignored
+   *   5 : style (see below)
+   * 
+   * The object can be a point (one vertex), a line (two vertices), or a
+   * triangle (three vertices).
+   * 
+   * The fill color for triangles is in 15-bit HiColor format with most
+   * significant bit zero, as follows:
+   * 
+   *   MSB ----------- LSB
+   *   0rrr rrgg gggb bbbb
+   * 
+   * For point objects, the style is an index into the point style
+   * array.
+   * 
+   * For line objects, the style is an index into the line style array.
+   * 
+   * For triangle objects, the style encodes three five-bit selectors
+   * and the most significant bit is zero, as follows:
+   * 
+   *   MSB ----------- LSB
+   *   0iii iijj jjjk kkkk
+   * 
+   * Each selector is either zero, or it is one greater than the index
+   * of style in the line style array.  This allows each selector to
+   * select either one of the first 31 styles in the line style array,
+   * or the special value of zero.
+   * 
+   * The first selector is for the triangle edge between the first and
+   * second vertices.  The second selector is for the triangle edge
+   * between the second and third vertices.  The third selector is for
+   * the triangle edge between the third and first vertices.  If the
+   * selector is zero, the edge will not be specially rendered.
+   * Otherwise, the edge will be rendered using the selected line style.
+   */
+  var m_scene;
+  
+  /*
+   * The painting sort array.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a Uint32Array with (n / 5) elements, where n is the number
+   * of elements in m_scene.  During a rendering operation, after vertex
+   * transformation, scene objects that survive the backface cull and
+   * full near/far plane clipping will be entered into this array.  Once
+   * all the surviving objects are entered into this array, the array
+   * will be sorted so that objects will be rendered back to front
+   * according to the painter's algorithm.
+   * 
+   * Each 32-bit integer stores a Z value in its most significant 16
+   * bits and an object index in its least significant 16 bits.  The
+   * object index is an index into the m_scene array, or it has the
+   * special value 0xffff, which means this array element is not used.
+   * All array elements that are not used are given the value 0xffffffff
+   * so that they are sorted at the end of the list.
+   * 
+   * The Z value is quantized so that zero is the far clipping plane and
+   * 0xffff is the near clipping plane.  This means that when the array
+   * is sorted in ascending numerical order, the farthest objects from
+   * the camera come first.
+   */
+  var m_paint;
+  
+  /*
+   * The point style array.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a regular array, where each array element is an object.
+   * These style objects are referenced from the style words of scene
+   * objects of point type in m_scene.
+   * 
+   * All point style objects have the following properties:
+   * 
+   *   "shape"  : string  - the shape to render at the given point
+   * 
+   *   "size"   : float   - edge length of bounding box
+   * 
+   *   "stroke" : float   - stroke width
+   * 
+   * The following shapes are supported:
+   * 
+   *    Property value |            Shape
+   *   ================+=============================
+   *          c        | Circle
+   *          s        | Square
+   *          m        | Diamond
+   *          u        | Triangle, pointing upwards
+   *          d        | Triangle, pointing downwards
+   *          l        | Triangle, pointing left
+   *          r        | Triangle, pointing right
+   *          p        | Plus sign +
+   *          x        | X
+   * 
+   * The size must be greater than 0.0.  It defines how large the
+   * rendered shape is.  This is the size when rendered in 2D,
+   * regardless of the Z distance.
+   * 
+   * The stroke width must either be 0.0 or a value greater than 0.0.
+   * If it is 0.0, the outline of the shape is not stroked.  Otherwise,
+   * it is stroked with this width when rendered in 2D, regardless of
+   * the Z distance.
+   * 
+   * All point styles for shapes that have an interior area (that is,
+   * all shapes except plus sign "p" and X "x") also have the following
+   * property:
+   * 
+   *   "fill" : integer - fill color or 0xffff for no fill
+   * 
+   * Fill color is encoded in 15-bit HiColor with most significant bit
+   * zero, as follows:
+   * 
+   *   MSB ----------- LSB
+   *   0rrr rrgg gggb bbbb
+   * 
+   * If the special 0xffff value is given, then the interior of the
+   * shape is left transparent.
+   * 
+   * If the "stroke" property of the point style is greater than zero,
+   * then the following property is also present:
+   * 
+   *   "ink" : integer - color to use for stroking the outline
+   * 
+   * The ink color is encoded in 15-bit HiColor with most significant
+   * bit zero, as follows:
+   * 
+   *   MSB ----------- LSB
+   *   0rrr rrgg gggb bbbb
+   */
+  var m_pstyle;
+  
+  /*
+   * The line style array.
+   * 
+   * Only available if m_loaded.
+   * 
+   * This is a regular array, where each array element is an object.
+   * These style objects are referenced from the style words of scene
+   * objects of line or triangle type in m_scene.  Line objects may
+   * reference any of the line styles, while triangle styles are only
+   * able to reference the first 31 line styles.
+   * 
+   * All line style objects have the following properties:
+   * 
+   *   "width" : float   - width of the line
+   * 
+   *   "color" : integer - color of the line
+   * 
+   * The width must be greater than zero.  It is the 2D rendered width
+   * of the line, regardless of the Z distance.
+   * 
+   * The color is encoded in 15-bit HiColor with most significant bit
+   * zero, as follows:
+   * 
+   *   MSB ----------- LSB
+   *   0rrr rrgg gggb bbbb
+   */
+  var m_lstyle;
+  
+  /*
    * Public functions
    * ================
    */
